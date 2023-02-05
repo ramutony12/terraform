8457381304410a5f01b10ea4f875202d14643a66
ramutony12 <116834915+ramutony12@users.noreply.github.com>
2023-02-05 21:49:24 +0530

    bbbb

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ secret_key ="fujRL9zuNWVE+yOsCYDcHRzyRyhar6MUaj4G9sPe"
 resouse "aws_instancs" "example"{
 ami   =ami-0aa7d40eeae50c9a9""
 instance_type="var.instance_type"
+ key_name="dell"
 tage= {
  name="krishna-tf"
  }
